344dc97fbc1ddde457d6e04a4f75b60be5c0afb3
Albert <albertonly898@gmail.com>
2018-12-27 22:15:36 +0700

    remove js plugin

--- resources/templates/back/footer.php
+++ resources/templates/back/footer.php
@@ -5,12 +5,8 @@
 <script src="js/jquery.js"></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.js"></script>
 
-<!-- Morris Charts JavaScript -->
-<script src="js/plugins/morris/raphael.min.js"></script>
-<script src="js/plugins/morris/morris.min.js"></script>
-<script src="js/plugins/morris/morris-data.js"></script>
 
 </body>
 
